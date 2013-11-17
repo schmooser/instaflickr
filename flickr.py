00a2c39ef2c214e363c6ad2f285fae82965f0a9d
Pavel Popov <pavelpopov@outlook.com>
2013-11-17 17:01:39 +0400

    Implemented simplest login via Flickr; Dropped Loginza

---
+++ flickr.py
@@ -0,0 +1,33 @@
+
+from hashlib import md5
+import urllib2
+from urllib import urlencode
+
+SECRET = 'flickr_secret_key'
+PARAMS = None
+
+def add_api_sig(params):
+    m = md5(SECRET)
+    m.update(''.join(sorted([str(x)+str(params[x]) \
+                              for x in params.keys()])))
+    api_sig = m.hexdigest()
+    params['api_sig'] = api_sig
+    return params
+    
+
+def request_method(method, values):
+    attrs = {}
+    for attr in PARAMS['methods'][method]:
+        if attr in PARAMS:
+            attrs[attr] = PARAMS[attr]
+        if attr in values:
+            attrs[attr] = values[attr]
+    attrs['method'] = method
+    attrs = add_api_sig(attrs)
+    url = PARAMS['urls']['rest']+'?'+urlencode(attrs)
+    response = urllib2.urlopen(url)
+    return response.read()
+
+
+    
+
--- instaflickr.py
+++ instaflickr.py
@@ -1,20 +1,50 @@
 
+import json
+from hashlib import md5
+import urlparse as urlparse  # replaced with url.parse in Python 3
+import urllib
 from flask import Flask
 from flask import render_template
+from flask import request
+
+import flickr
+
+
 app = Flask(__name__)
+
+params = json.load(open('instaflickr.json'))
+
+flickr.SECRET = params['flickr']['secret']
+flickr.PARAMS = params['flickr']
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello(name=None):
-    return render_template('block.html', name=name)
+    return render_template('block.html', name=name, params=params)
+
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    frob = request.args.get('frob', None)
+    if frob is None:  # displaying login page
+        url = params['flickr']['urls']['auth']
+        url_params = {'api_key': params['flickr']['api_key'],
+                      'perms': params['flickr']['perms']}
+        url_params = flickr.add_api_sig(url_params)
+        query = urllib.urlencode(url_params)
+
+        return render_template('login.html', params=params,
+                               flickr_url=url+'?'+query)
+    else:  # check auth from flickr
+        response = flickr.request_method('flickr.auth.getToken',
+                                         {'frob': frob})
+        return render_template('login.html', params=params,
+                                response=response)
 
 if __name__ == "__main__":
     app.debug = True
